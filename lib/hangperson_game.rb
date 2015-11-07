8e2328987dbbda322b97cc4036839d99520d63d8
steven1l2 <steven1l2@yahoo.com>
2015-11-07 04:30:01 +0000

    First Commit

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,22 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :wrong_guesses
+  attr_accessor :guesses
+  
+  @@correct_guess = Array.new
+  @@repeat_array = Array.new
+  @@wrong_guess_list = Array.new
+  @@guess_hash = Hash.new
+
+  
+  
+  
   def initialize(word)
     @word = word
+    @wrong_guesses = ''
+    @guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +32,25 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+
+  def guess(letter)
+
+    @guesses = letter
+    @guesses_temp = letter
+   
+    
+    if((@guesses == nil) || (@guesses == '') || !(@guesses =~ /[A-Za-z]/))
+     raise ArgumentError.new("Incorrect input")
+    end
+
+    
+    
+    
+    
+    
+    
+  end
+
+
 
 end
