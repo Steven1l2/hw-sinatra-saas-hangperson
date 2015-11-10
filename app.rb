6208600dc616e14eda5434f459618b4e677cbed9
steven1l2 <steven1l2@yahoo.com>
2015-11-10 13:22:04 +0000

    Sending to github

--- app.rb
+++ app.rb
@@ -15,7 +15,6 @@ class HangpersonApp < Sinatra::Base
     session[:game] = @game
   end
   
-  
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
   get '/' do
@@ -40,9 +39,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    
     @game.guess(letter)
-    
     
     redirect '/show'
   end
@@ -52,18 +49,26 @@ class HangpersonApp < Sinatra::Base
   # won, lost, or neither, and take the appropriate action.
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
+  
   get '/show' do
     ### YOUR CODE HERE ###
+    #@game.wrong_guesses
+    #@game.word_with_guesses
+    #redirect '/show'
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    #@game.check_win_or_lose
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    
+    #@game.check_win_or_lose
+    
     erb :lose # You may change/remove this line
   end
   
